5157c3d341c42367eed21701cda29fb5b06b8223
Ngo Hong Thai <ngohongthai.uet@gmail.com>
2022-12-29 14:26:31 +0700

    Add helper functions for saving log

--- engine.py
+++ engine.py
@@ -122,7 +122,9 @@ def train(model: torch.nn.Module,
           optimizer: torch.optim.Optimizer,
           loss_fn: torch.nn.Module,
           epochs: int,
-          device: torch.device) -> Dict[str, List]:
+          device: torch.device, 
+          writer: torch.utils.tensorboard.writer.SummaryWriter # new parameter to take in a writer
+          ) -> Dict[str, List]:
     """Trains and tests a PyTorch model.
 
     Passes a target PyTorch models through train_step() and test_step()
@@ -131,28 +133,31 @@ def train(model: torch.nn.Module,
 
     Calculates, prints and stores evaluation metrics throughout.
 
+    Stores metrics to specified writer log_dir if present.
+
     Args:
-    model: A PyTorch model to be trained and tested.
-    train_dataloader: A DataLoader instance for the model to be trained on.
-    test_dataloader: A DataLoader instance for the model to be tested on.
-    optimizer: A PyTorch optimizer to help minimize the loss function.
-    loss_fn: A PyTorch loss function to calculate loss on both datasets.
-    epochs: An integer indicating how many epochs to train for.
-    device: A target device to compute on (e.g. "cuda" or "cpu").
+      model: A PyTorch model to be trained and tested.
+      train_dataloader: A DataLoader instance for the model to be trained on.
+      test_dataloader: A DataLoader instance for the model to be tested on.
+      optimizer: A PyTorch optimizer to help minimize the loss function.
+      loss_fn: A PyTorch loss function to calculate loss on both datasets.
+      epochs: An integer indicating how many epochs to train for.
+      device: A target device to compute on (e.g. "cuda" or "cpu").
+      writer: A SummaryWriter() instance to log model results to.
 
     Returns:
-    A dictionary of training and testing loss as well as training and
-    testing accuracy metrics. Each metric has a value in a list for 
-    each epoch.
-    In the form: {train_loss: [...],
-              train_acc: [...],
-              test_loss: [...],
-              test_acc: [...]} 
-    For example if training for epochs=2: 
-             {train_loss: [2.0616, 1.0537],
-              train_acc: [0.3945, 0.3945],
-              test_loss: [1.2641, 1.5706],
-              test_acc: [0.3400, 0.2973]} 
+      A dictionary of training and testing loss as well as training and
+      testing accuracy metrics. Each metric has a value in a list for 
+      each epoch.
+      In the form: {train_loss: [...],
+                train_acc: [...],
+                test_loss: [...],
+                test_acc: [...]} 
+      For example if training for epochs=2: 
+              {train_loss: [2.0616, 1.0537],
+                train_acc: [0.3945, 0.3945],
+                test_loss: [1.2641, 1.5706],
+                test_acc: [0.3400, 0.2973]} 
     """
     # Create empty results dictionary
     results = {"train_loss": [],
@@ -160,9 +165,6 @@ def train(model: torch.nn.Module,
                "test_loss": [],
                "test_acc": []
     }
-    
-    # Make sure model on target device
-    model.to(device)
 
     # Loop through training and testing steps for a number of epochs
     for epoch in tqdm(range(epochs)):
@@ -190,6 +192,23 @@ def train(model: torch.nn.Module,
         results["train_acc"].append(train_acc)
         results["test_loss"].append(test_loss)
         results["test_acc"].append(test_acc)
+
+        # See if there's a writer, if so, log to it
+        if writer:
+            # Add results to SummaryWriter
+            writer.add_scalars(main_tag="Loss", 
+                               tag_scalar_dict={"train_loss": train_loss,
+                                                "test_loss": test_loss},
+                               global_step=epoch)
+            writer.add_scalars(main_tag="Accuracy", 
+                               tag_scalar_dict={"train_acc": train_acc,
+                                                "test_acc": test_acc}, 
+                               global_step=epoch)
+
+            # Close the writer
+            writer.close()
+        else:
+            pass
 
     # Return the filled results at the end of the epochs
     return results

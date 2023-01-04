5dd1a60f192ab1c12344dc8cfa3e5adb55f0594d
Ngo Hong Thai <ngohongthai.uet@gmail.com>
2023-01-04 19:58:20 +0700

    updating code

--- data_setup.py
+++ data_setup.py
@@ -2,97 +2,63 @@
 Contains functionality for creating Tensorflow Datasets
 """
 
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
 
 BATCH_SIZE = 32
-CLASS_MODE = "binary" # "categorical" or "binary", "sparse", "input", None
-TARGET_SIZE = (224, 224)
+IMAGE_SIZE = (224, 224)
+TRAIN_SUBSET = "train"
+VALIDATION_SUBSET = "test"
+LABEL_MODE = "categorical" # "binary" or "categorical"
+seed = 123
 
-default_train_datagen = ImageDataGenerator(rescale=1/255.)
-default_test_datagen = ImageDataGenerator(rescale=1/255.) 
+def _build_dataset(data_dir, subset, image_size):
+  return tf.keras.preprocessing.image_dataset_from_directory(
+      data_dir,
+      validation_split=.20,
+      subset=subset,
+      label_mode=LABEL_MODE,
+      seed=seed,
+      image_size=image_size,
+      batch_size=1)
 
-train_datagen_with_augmentation = ImageDataGenerator(rescale=1/255.,
-                                             rotation_range=20, # rotate the image slightly between 0 and 20 degrees (note: this is an int not a float)
-                                             shear_range=0.2, # shear the image
-                                             zoom_range=0.2, # zoom into the image
-                                             width_shift_range=0.2, # shift the image width ways
-                                             height_shift_range=0.2, # shift the image height ways
-                                             horizontal_flip=True) # flip the image on the horizontal axis
+def build_train_dataset(data_dir, 
+                        image_size=IMAGE_SIZE, 
+                        batch_size=BATCH_SIZE, 
+                        do_data_augmentation=False):
+  train_ds = _build_dataset(data_dir, TRAIN_SUBSET, image_size)
+  class_names = tuple(train_ds.class_names)
+  train_size = train_ds.cardinality().numpy()
+  train_ds = train_ds.unbatch().batch(batch_size)
+  train_ds = train_ds.repeat()
 
+  normalization_layer = tf.keras.layers.Rescaling(1. / 255)
+  preprocessing_model = tf.keras.Sequential([normalization_layer])
+  if do_data_augmentation:
+    preprocessing_model.add(
+        tf.keras.layers.RandomRotation(40))
+    preprocessing_model.add(
+        tf.keras.layers.RandomTranslation(0, 0.2))
+    preprocessing_model.add(
+        tf.keras.layers.RandomTranslation(0.2, 0))
+    # Like the old tf.keras.preprocessing.image.ImageDataGenerator(),
+    # image sizes are fixed when reading, and then a random zoom is applied.
+    # If all training inputs are larger than image_size, one could also use
+    # RandomCrop with a batch size of 1 and rebatch later.
+    preprocessing_model.add(
+        tf.keras.layers.RandomZoom(0.2, 0.2))
+    preprocessing_model.add(
+        tf.keras.layers.RandomFlip(mode="horizontal"))
+  train_ds = train_ds.map(lambda images, labels:
+                          (preprocessing_model(images), labels))
+  return train_ds, train_size, class_names
 
-def create_datasets(
-    train_dir: str, 
-    test_dir: str,
-    train_datagen: ImageDataGenerator = default_train_datagen,
-    test_datagen: ImageDataGenerator = default_test_datagen,
-    target_size:(int, int) = TARGET_SIZE,
-    batch_size: int = BATCH_SIZE, 
-    class_mode: str = CLASS_MODE, # categorical or binary
-    shuffle: bool = True,
-):
-  """Creates training and testing datasets.
-
-  Takes in a training directory and testing directory path and turns
-  them into tensorflow datasets
-
-  Args:
-    train_dir: Path to training directory.
-    test_dir: Path to testing directory.
-    train_datagen: tensorflow transform to perform on training data.
-    test_datagen: tensorflow transform to perform on testing data.
-    batch_size: Number of samples per batch in each of the dataset.
-    target_size: Image size after transformation.
-    class_mode: One of "categorical", "binary",
-    shuffle: Whether to shuffle the data.
-
-  Returns:
-    A tuple of (train_datasets, test_datasets).
-  """
-
-  train_data = train_datagen.flow_from_directory(train_dir,
-                                               target_size=target_size,
-                                               batch_size=batch_size,
-                                               class_mode=class_mode,
-                                               shuffle=shuffle) 
-  
-  test_data = test_datagen.flow_from_directory(test_dir,
-                                             target_size=target_size,
-                                             batch_size=batch_size,
-                                             class_mode=class_mode,
-                                             shuffle=False)
-  
-  return train_data, test_data
-
-def create_datasets_with_augmentation(
-    train_dir: str, 
-    test_dir: str,
-    target_size:(int, int) = TARGET_SIZE,
-    batch_size: int = BATCH_SIZE, 
-    class_mode: str = CLASS_MODE, # categorical or binary
-    shuffle: bool = True,):
-  return create_datasets(
-    train_dir = train_dir, 
-    test_dir = test_dir,
-    train_datagen = train_datagen_with_augmentation,
-    test_datagen = default_test_datagen,
-    target_size = target_size,
-    batch_size = batch_size, 
-    class_mode = class_mode, # categorical or binary
-    shuffle = shuffle)
-
-def create_datasets_without_augmentation(
-    train_dir: str, 
-    test_dir: str,
-    target_size:(int, int) = TARGET_SIZE,
-    batch_size: int = BATCH_SIZE, 
-    class_mode: str = CLASS_MODE, # categorical or binary
-    shuffle: bool = True,):
-  return create_datasets(
-    train_dir = train_dir, 
-    test_dir = test_dir,
-    train_datagen = default_train_datagen,
-    test_datagen = default_test_datagen,
-    target_size = target_size,
-    batch_size = batch_size, 
-    class_mode = class_mode, # categorical or binary
-    shuffle = shuffle)
+def buil_validation_dataset(data_dir,
+                            image_size=IMAGE_SIZE,
+                            batch_size=BATCH_SIZE):
+  val_ds = _build_dataset(data_dir, VALIDATION_SUBSET, image_size)
+  normalization_layer = tf.keras.layers.Rescaling(1. / 255)
+  valid_size = val_ds.cardinality().numpy()
+  val_ds = val_ds.unbatch().batch(image_size)
+  val_ds = val_ds.map(lambda images, labels:
+                      (normalization_layer(images), labels))
+  return val_ds, valid_size
--- utils.py
+++ utils.py
@@ -1,145 +1,35 @@
 """
-Contains various utility functions for PyTorch.
+Contains various utility functions for Tensorflow.
 """
+import os
+import glob
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
 from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
-import os
-import zipfile
-import requests
 from typing import List
-from PIL import Image
-import numpy as np
+#from PIL import Image
 from sklearn.metrics import confusion_matrix
 import datetime
 
+def print_tree(root, level=0):
+    indent = ' ' * 6 * level
+    num_files = len(glob.glob(f'{root}/*'))
+    print(f'{indent}{os.path.basename(root)}/ ({num_files} files)')
+    subindent = ' ' * 6 * (level + 1)
+    for f in glob.glob(f'{root}/*'):
+        if os.path.isdir(f):
+            print_tree(f, level + 1)
 
-def download_data(source: str, 
-                  destination: str,
-                  remove_source: bool = True) -> Path:
-    """Downloads a zipped dataset from source and unzips to destination.
-
-    Args:
-        source (str): A link to a zipped file containing data.
-        destination (str): A target directory to unzip data to.
-        remove_source (bool): Whether to remove the source after downloading and extracting.
-    
-    Returns:
-        pathlib.Path to downloaded data.
-    
-    Example usage:
-        download_data(source="https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi.zip",
-                      destination="pizza_steak_sushi")
-    """
-    # Setup path to data folder
-    data_path = Path("data/")
-    image_path = data_path / destination
-
-    # If the image folder doesn't exist, download it and prepare it... 
-    if image_path.is_dir():
-        print(f"[INFO] {image_path} directory exists, skipping download.")
-    else:
-        print(f"[INFO] Did not find {image_path} directory, creating one...")
-        image_path.mkdir(parents=True, exist_ok=True)
-        # Download pizza, steak, sushi data
-        target_file = Path(source).name
-        with open(data_path / target_file, "wb") as f:
-            request = requests.get(source)
-            print(f"[INFO] Downloading {target_file} from {source}...")
-            f.write(request.content)
-
-        # Unzip pizza, steak, sushi data
-        with zipfile.ZipFile(data_path / target_file, "r") as zip_ref:
-            print(f"[INFO] Unzipping {target_file} data...") 
-            zip_ref.extractall(data_path)
-
-        # Remove .zip file
-        if remove_source:
-            os.remove(data_path / target_file)
-    
-    return image_path
-
-def walk_through_dir(dir_path):
-    """
-    Walks through dir_path returning its contents.
-    Args:
-    dir_path (str): target directory
-
-    Returns:
-    A print out of:
-      number of subdiretories in dir_path
-      number of images (files) in each subdirectory
-      name of each subdirectory
-    """
-    for dirpath, dirnames, filenames in os.walk(dir_path):
-        if (len(filenames) > 0):
-            print(f"There are {len(filenames)} images in '{dirpath}'.")
-
-def detail_dataset(train_dir: str, test_dir: str):
-    all_classes = os.listdir(train_dir)
-    print("There are {} classes in the dataset".format(len(all_classes)))
-    print("="*50)
-    title = ['class', 'train', 'test']
-    print("{:<20} {:<20} {:<20}".format(*title))
-    print("-"*50)
-    total_train = 0
-    total_test = 0
-    for i in all_classes:
-        print("{:<20} {:<20} {:<20}".format(i, len(os.listdir(train_dir+"/"+i)), len(os.listdir(test_dir+"/"+i))))
-        total_test += len(os.listdir(test_dir+"/"+i))
-        total_train += len(os.listdir(train_dir+"/"+i))
-    print("-"*50)
-    print("{:<20} {:<20} {:<20}".format("Total", total_train, total_test))
-            
-def get_random_images(num_images: int, 
-                      data_path: str, 
-                      seed: int = 42) -> List[str]:
-    """Gets a list of random images from a directory.
-
-    Args:
-        num_images (int): number of random images to return.
-        data_path (str): target directory to get random images from.
-        seed (int, optional): random seed. Defaults to 42.
-    
-    Returns:
-        List of random images from a directory.
-    
-    Example usage:
-        get_random_images(num_images=5,
-                          data_path="pizza_steak_sushi")
-    """
-    # Set random seed
-    np.random.seed(seed)
-
-    # Get all the images in data_path
-    all_images = [image_path for image_path in Path(data_path).rglob("*.*")]
-
-    # Get random images
-    random_images = np.random.choice(all_images, size=num_images, replace=False)
-
-    return random_images
 
 def plot_random_images(num_images: int, 
                        data_path: str, 
                        seed: int = 42) -> None:
-    """Plots a number of random images from a directory.
-
-    Args:
-        num_images (int): number of random images to plot.
-        data_path (str): target directory to get random images from.
-        seed (int, optional): random seed. Defaults to 42.
-    
-    Example usage:
-        plot_random_images(num_images=5,
-                           data_path="pizza_steak_sushi")
-    """
+    np.random.seed(seed)
+    # Get all the images in data_path
+    all_images = [image_path for image_path in Path(data_path).rglob("*.*")]
     # Get random images
-    random_images = get_random_images(num_images=num_images,
-                                      data_path=data_path,
-                                      seed=seed)
+    random_images = np.random.choice(all_images, size=num_images, replace=False)
 
     # Plot random images
     plt.figure(figsize=(15, 15))
@@ -150,16 +40,6 @@ def plot_random_images(num_images: int,
         plt.title(image_path.parent.name)
         plt.axis("off")
 
-def plot_random_augmented_image(num_images: int,
-                                datasets: tf.keras.preprocessing.image.DirectoryIterator):
-    plt.figure(figsize=(15, 15))
-    for i in range(num_images):
-        images, labels = datasets.next()
-        random_number = np.random.randint(0, images.shape[0])
-        ax = plt.subplot(1, num_images, i+1)
-        plt.imshow(images[random_number])
-        #plt.title(list(datasets.class_indices.keys())[int(labels[random_number])])
-        plt.axis("off")
     
 # Plot loss curves of a model
 def plot_loss_curves(results):
@@ -274,16 +154,6 @@ def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_s
         fig.savefig("confusion_matrix.png")
         
 def create_tensorboard_callback(dir_name, experiment_name):
-    """
-    Creates a TensorBoard callback instand to store log files.
-
-    Stores log files with the filepath:
-        "dir_name/experiment_name/current_datetime/"
-
-    Args:
-        dir_name: target directory to store TensorBoard log files
-        experiment_name: name of experiment directory (e.g. efficientnet_model_1)
-    """
     log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     tensorboard_callback = tf.keras.callbacks.TensorBoard(
         log_dir=log_dir
@@ -292,15 +162,6 @@ def create_tensorboard_callback(dir_name, experiment_name):
     return tensorboard_callback
 
 def compare_historys(original_history, new_history, initial_epochs=5):
-    """
-    Compares two TensorFlow model History objects.
-    
-    Args:
-      original_history: History object from original model (before new_history)
-      new_history: History object from continued model training (after original_history)
-      initial_epochs: Number of epochs in original_history (new_history plot starts from here) 
-    """
-    
     # Get original history measurements
     acc = original_history.history["accuracy"]
     loss = original_history.history["loss"]

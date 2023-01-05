d95cb6dae46d9ff1f90f235cd1314157251fde01
Ngo Hong Thai <ngohongthai.uet@gmail.com>
2023-01-06 01:38:46 +0700

    Update helper files

--- tf_cv_helper.py
+++ tf_cv_helper.py
@@ -4,6 +4,7 @@
 __version__ = "0.0.1"
 __author__ = "Ngo Hong Thai"
 
+
 ############# Setup environment #############
 import os
 import glob
@@ -245,6 +246,15 @@ def get_base_model(model_name, trainable=False):
     return base_model
 
 def create_preprocessing_layer(data_augmentation=False, data_normalization=True):
+    """ Tạo preprocessing layer bao gồm data augmentation và data normalization
+
+    Args:
+        data_augmentation (bool, optional): Có sử dụng data augmentation hay không. Defaults to False.
+        data_normalization (bool, optional): Có sử dụng data normalization hay không. Defaults to True.
+    Returns:
+        Layer: Preprocessing layer
+    """
+    
     preprocessing_layers = Sequential([], name="preprocessing_layers")
     if data_augmentation:
         preprocessing_layers.add(preprocessing.RandomFlip('horizontal'))
@@ -258,7 +268,192 @@ def create_preprocessing_layer(data_augmentation=False, data_normalization=True)
     return preprocessing_layers
 
 def create_input_layer(model_name):
+    """ Tạo input layer
+
+    Args:
+        model_name (str): Tên của model
+        
+    Returns:
+        Layer: Input layer
+    """
+    
     image_size = get_model_input_size(model_name)
     input_layer = layers.Input(shape=image_size + (3,), name="input_layer")
+    
+    return input_layer
 
 ###### Các hàm hỗ trợ đánh giá model, report ... #####
+def plot_loss_curves(results):
+    """Plots training curves of a results dictionary.
+
+    Args:
+        results (dict): dictionary containing list of values, e.g.
+            {"loss": [...],
+             "accuracy": [...],
+             "val_loss": [...],
+             "val_accuracy": [...]}
+    """
+    loss = results["loss"]
+    test_loss = results["accuracy"]
+
+    accuracy = results["val_loss"]
+    test_accuracy = results["val_accuracy"]
+
+    epochs = range(len(results["loss"]))
+
+    plt.figure(figsize=(15, 7))
+
+    # Plot loss
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss, label="train_loss")
+    plt.plot(epochs, test_loss, label="test_loss")
+    plt.title("Loss")
+    plt.xlabel("Epochs")
+    plt.legend()
+
+    # Plot accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, accuracy, label="train_accuracy")
+    plt.plot(epochs, test_accuracy, label="test_accuracy")
+    plt.title("Accuracy")
+    plt.xlabel("Epochs")
+    plt.legend()
+    
+# Our function needs a different name to sklearn's plot_confusion_matrix
+def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=15, norm=False, savefig=False): 
+    """Makes a labelled confusion matrix comparing predictions and ground truth labels.
+
+    If classes is passed, confusion matrix will be labelled, if not, integer class values
+    will be used.
+
+    Args:
+        y_true: Array of truth labels (must be same shape as y_pred).
+        y_pred: Array of predicted labels (must be same shape as y_true).
+        classes: Array of class labels (e.g. string form). If `None`, integer labels are used.
+        figsize: Size of output figure (default=(10, 10)).
+        text_size: Size of output figure text (default=15).
+        norm: normalize values or not (default=False).
+        savefig: save confusion matrix to file (default=False).
+    
+    Returns:
+        A labelled confusion matrix plot comparing y_true and y_pred.
+
+    Example usage:
+        make_confusion_matrix(y_true=test_labels, # ground truth test labels
+                            y_pred=y_preds, # predicted labels
+                            classes=class_names, # array of class label names
+                            figsize=(15, 15),
+                            text_size=10)
+    """  
+    # Create the confustion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis] # normalize it
+    n_classes = cm.shape[0] # find the number of classes we're dealing with
+
+    # Plot the figure and make it pretty
+    fig, ax = plt.subplots(figsize=figsize)
+    cax = ax.matshow(cm, cmap=plt.cm.Blues) # colors will represent how 'correct' a class is, darker == better
+    fig.colorbar(cax)
+
+    # Are there a list of classes?
+    if classes:
+        labels = classes
+    else:
+        labels = np.arange(cm.shape[0])
+    
+    # Label the axes
+    ax.set(title="Confusion Matrix",
+            xlabel="Predicted label",
+            ylabel="True label",
+            xticks=np.arange(n_classes), # create enough axis slots for each class
+            yticks=np.arange(n_classes), 
+            xticklabels=labels, # axes will labeled with class names (if they exist) or ints
+            yticklabels=labels)
+    
+    # Make x-axis labels appear on bottom
+    ax.xaxis.set_label_position("bottom")
+    ax.xaxis.tick_bottom()
+
+    # Set the threshold for different colors
+    threshold = (cm.max() + cm.min()) / 2.
+
+    # Plot the text on each cell
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if norm:
+            plt.text(j, i, f"{cm[i, j]} ({cm_norm[i, j]*100:.1f}%)",
+                horizontalalignment="center",
+                color="white" if cm[i, j] > threshold else "black",
+                size=text_size)
+        else:
+            plt.text(j, i, f"{cm[i, j]}",
+                horizontalalignment="center",
+                color="white" if cm[i, j] > threshold else "black",
+                size=text_size)
+
+    # Save the figure to the current working directory
+    if savefig:
+        fig.savefig("confusion_matrix.png")
+        
+def create_tensorboard_callback(dir_name, experiment_name):
+    log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(
+        log_dir=log_dir
+    )
+    print(f"Saving TensorBoard log files to: {log_dir}")
+    return tensorboard_callback
+
+
+def compare_historys(original_history, new_history, initial_epochs=5):
+    # Get original history measurements
+    acc = original_history.history["accuracy"]
+    loss = original_history.history["loss"]
+
+    val_acc = original_history.history["val_accuracy"]
+    val_loss = original_history.history["val_loss"]
+
+    # Combine original history with new history
+    total_acc = acc + new_history.history["accuracy"]
+    total_loss = loss + new_history.history["loss"]
+
+    total_val_acc = val_acc + new_history.history["val_accuracy"]
+    total_val_loss = val_loss + new_history.history["val_loss"]
+
+    # Make plots
+    plt.figure(figsize=(8, 8))
+    plt.subplot(2, 1, 1)
+    plt.plot(total_acc, label='Training Accuracy')
+    plt.plot(total_val_acc, label='Validation Accuracy')
+    plt.plot([initial_epochs-1, initial_epochs-1],
+              plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(2, 1, 2)
+    plt.plot(total_loss, label='Training Loss')
+    plt.plot(total_val_loss, label='Validation Loss')
+    plt.plot([initial_epochs-1, initial_epochs-1],
+              plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('epoch')
+    plt.show()
+    
+def calculate_results(y_true, y_pred):
+    """
+    Calculates model accuracy, precision, recall and f1 score of a binary classification model.
+
+    Args:
+        y_true: true labels in the form of a 1D array
+        y_pred: predicted labels in the form of a 1D array
+
+    Returns a dictionary of accuracy, precision, recall, f1-score.
+    """
+    # Calculate model accuracy
+    model_accuracy = accuracy_score(y_true, y_pred) * 100
+    # Calculate model precision, recall and f1 score using "weighted average
+    model_precision, model_recall, model_f1, _ = precision_recall_fscore_support(y_true, y_pred, average="weighted")
+    model_results = {"accuracy": model_accuracy,
+                    "precision": model_precision,
+                    "recall": model_recall,
+                    "f1": model_f1}
+    return model_results

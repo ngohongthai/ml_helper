f1f8ebbecd2575afb2cb541628ce06caec651e32
Ngo Hong Thai <ngohongthai.uet@gmail.com>
2023-01-09 09:24:07 +0700

    Update tf_cv_helper.py

--- tf_cv_helper.py
+++ tf_cv_helper.py
@@ -26,7 +26,7 @@ def print_env_info():
     """ Print environment information
     
     """
-    
+    print("################ ENVIRONMENT ################")
     print(f"Python Platform: {platform.platform()}")
     print(f"Python {sys.version}")
     print(f"Tensor Flow Version: {tf.__version__}")
@@ -35,6 +35,7 @@ def print_env_info():
 
     gpu = len(tf.config.list_physical_devices('GPU'))>0
     print("GPU is", "available" if gpu else "NOT AVAILABLE")
+    print("#############################################")
 
 ##### Các hàm hỗ trợ download data, visualize data ... #####
 
